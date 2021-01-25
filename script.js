00c896fbd7f5dd046a69d5b723473fe0242d4c0d
Jake Kravas <jakekravas7@gmail.com>
2021-01-25 12:07:02 -0700

    cleaned things upt

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-document.addEventListener("DOMContentLoaded", function(){
+$("#data-form").submit((e) => {
+  e.preventDefault();
 
-  $("#data-form").submit(function (e) {
-    e.preventDefault();
-
-    $.ajax({
-      type: "POST",
-      url: "http://34.198.60.157:3300/api/userid",
-      data: $("#data-input").serialize(),
-      success: function (msg) {
-        window.location.href="http://34.198.60.157:4200/"
-      },
-      error: function () {
-        alert("error");
-      }
-    });
+  $.ajax({
+    type: "POST",
+    url: "http://34.198.60.157:3300/api/userid",
+    data: $("#data-input").serialize(),
+    success: function () {
+      window.location.href="http://34.198.60.157:4200/"
+    },
+    error: function (err) {
+      alert(`ERROR: ${err}`);
+    }
   });
 });
