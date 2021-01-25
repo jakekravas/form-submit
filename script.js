47af8f9631f8db722dbc0ab39b5d60f7351abd89
Jake Kravas <jakekravas7@gmail.com>
2021-01-25 12:16:06 -0700

    .

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$("#data-form").submit((e) => {
+$('#data-form').submit(e => {
   e.preventDefault();
 
   $.ajax({
-    type: "POST",
-    url: "https://34.198.60.157:3300/api/userid",
-    data: $("#data-input").serialize(),
-    success: function () {
-      window.location.href="https://34.198.60.157:4200/";
+    type: 'POST',
+    url: 'http://34.198.60.157:3300/api/userid',
+    data: $('#data-input').serialize(),
+    success: () => {
+      window.location.href='http://34.198.60.157:4200/';
     },
-    error: function (err) {
+    error: err => {
       alert(`ERROR: ${err}`);
     }
   });
